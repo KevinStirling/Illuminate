f6860ae5d6d5043fbfaf3c7a6bab1b534825486c
bscalera <Scalerab1@mail.montclair.edu>
2015-03-25 16:56:18 -0400

    switch statement
    I think this should use a "switch" statement instead of using "if".  If
    this works and there are no problems with this, I will add more colors
    to it.
    https://wiki.python.org/moin/SwitchStatement

--- ColorLibrary.py
+++ ColorLibrary.py
@@ -2,22 +2,36 @@ class Colors:
 	
 	#we can add more colors here
 	def returnRGBVal(self, color):
-		if color == 'yellow':
-			return "225,225,0"
-		elif color == 'red':
-			return "225,0,0"
-		elif color == 'lime':
-			return "0,225,0"
-		elif color == 'blue':
-			return "0,0,225"
-		elif color == 'green':
-			return "0,128,0"
-		elif color == 'purple':
-			return "128,0,128"
-		elif color == 'navy':
-			return "0,0,128"
-		else:
-			return 'test'
+#		if color == 'yellow':
+#			return "225,225,0"
+#		elif color == 'red':
+#			return "225,0,0"
+#		elif color == 'lime':
+#			return "0,225,0"
+#		elif color == 'blue':
+#			return "0,0,225"
+#		elif color == 'green':
+#			return "0,128,0"
+#		elif color == 'purple':
+#			return "128,0,128"
+#		elif color == 'navy':
+#			return "0,0,128"
+#		else:
+#			return 'test'
+
+
+	switch(color)
+	{
+  		case'yellow': {return "225,225,0";}
+  		case'red': {return "225,0,0";}
+		case'lime': {return "0,225,0";}
+  		case'blue': {return "0,0,225";}
+		case'green': {return "0,128,0";}
+  		case'purple': {return "128,0,128";}
+		case'navy': {return "0,0,128";}
+  		default: {return 'test';}
+	}
+
 	
 	#process string
 	def returnRed(self,RGBVal):
