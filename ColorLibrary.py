be283c653f63b987813f5a99c8500527a10f3c20
bscalera <Scalerab1@mail.montclair.edu>
2015-03-26 14:55:55 -0400

    Add more colors
    Get colors from http://www.cloford.com/resources/colours/500col.htm

--- ColorLibrary.py
+++ ColorLibrary.py
@@ -20,6 +20,28 @@ class Colors:
 		else:
 			return 'not a valid color'
 
+		
+
+	#Get colors from http://www.cloford.com/resources/colours/500col.htm
+		if color == 'indian red':
+			return "176,23,31"
+		elif color == 'crimson':
+			return "220,20,60"
+		elif color == 'lightpink':
+			return "255,182,193"
+		elif color == 'lightpink 1':
+			return "255,174,185"
+		elif color == 'lightpink 2':
+			return "238,162,173"
+		elif color == 'lightpink 3':
+			return "205,140,149"
+		elif color == 'lightpink 4':
+			return "139,95,101"
+		else:
+			return 'not a valid color'
+
+
+
 	
 	#process string
 	def returnRed(self,RGBVal):
